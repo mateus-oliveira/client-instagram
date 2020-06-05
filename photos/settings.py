126180765b5d49d510c8213bde9368ed28d37736
mateus-oliveira <matews5522@gmail.com>
2020-06-05 08:46:34 -0300

    Server Https

--- photos/settings.py
+++ photos/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'sslserver',
     'social_django',
     'profile_grid',
 ]
