632d55a846c83afdd7ac21f0a690b0393a2ca217
mateus-oliveira <matews5522@gmail.com>
2020-06-05 08:07:56 -0300

    Deploy on Heroku

--- photos/settings.py
+++ photos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ except ImportError:
 
 
 LOGIN_REDIRECT_URL = '/accounts/profile/'
+
+django_on_heroku.settings(locals())
